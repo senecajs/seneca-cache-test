aad6992f49c7621d69c665d00aebbb76fd7ac60c
Richard Rodger <richard@ricebridge.com>
2014-10-14 22:30:22 +0100

    initial 0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-seneca-cache-test
-=================
+seneca-cache-test - a [Seneca](http://senecajs.org) plugin
+======================================================
 
-seneca-cache-test
+## Seneca Cache-Test Plugin
+
+This module provides a standard set of tests for Seneca caches. It is used to verify that a cache meets the minimum requirements needed for the Seneca cache message patterns. See the [seneca-cache](http://github.com/darsee/seneca-cache) article for more information.
+
+[![Build Status](https://travis-ci.org/rjrodger/seneca-cache-test.png?branch=master)](https://travis-ci.org/rjrodger/seneca-cache-test)
+
+[![NPM](https://nodei.co/npm/seneca-cache-test.png)](https://nodei.co/npm/seneca-cache-test/)
+[![NPM](https://nodei.co/npm-dl/seneca-cache-test.png)](https://nodei.co/npm-dl/seneca-cache-test/)
+
+For a gentle introduction to Seneca itself, see the
+[senecajs.org](http://senecajs.org) site.
+
+If you're using this plugin module, feel free to contact me on twitter if you
+have any questions! :) [@rjrodger](http://twitter.com/rjrodger)
+
+Current Version: 0.1.0
+
+Tested on: Seneca 0.5.21, Node 0.10.31
+
+
+### Usage
+
+This module is included as a development dependency by Seneca cache plugins. For a simple example, see the [seneca-memcached-cache](https://github.com/darsee/seneca-memcached-cache) plugin test cases.
+
+
---
+++ cache-test.js
@@ -0,0 +1,119 @@
+/* Copyright (c) 2014 Richard Rodger, MIT License */
+"use strict";
+
+
+var assert = require('assert')
+
+var async = require('async')
+
+
+exports.basictest = function(si,done) {
+
+  si.ready(function(){
+    console.log('BASIC')
+    assert.ok( si )
+
+    var cache = si.pin({role: 'cache', cmd: '*'});
+
+    async.series(
+      {
+        set0: function(fin) {
+          cache.set({key: 'a', val: '1'}, function(err, out) {
+            if(err) return fin(err);
+            assert(out, 'a');
+            fin();
+          });
+        },
+
+        get0: function(fin) {
+          cache.get({key: 'a'}, function(err, out) {
+            if(err) return fin(err);
+            assert.equal(out, '1');
+            fin();
+          });
+        },
+
+
+        add0: function(fin) {
+          cache.add({key: 'b', val: 1}, function(err, out) {
+            if(err) return fin(err);
+            assert.equal(out, 'b');
+            fin();
+          });
+        },
+
+        refuse_existing0: function(fin) {
+          cache.add({key: 'b', val: 'something'}, function(err, out) {
+            if(err) return fin(err);
+
+            cache.get({key: 'b'}, function(err, out) {
+              if(err) return fin(err);
+              assert.equal(out, 1);
+              fin();
+            });
+          });
+        },
+
+        incr0: function(fin) {
+          cache.incr({key: 'b', val: 4}, function(err, out) {
+            if(err) return fin(err);
+
+            assert.equal(out, 5);
+            fin();
+          });
+        },
+
+        decr0: function(fin) {
+          cache.decr({key: 'b', val: 3}, function(err, out) {
+            if(err) return fin(err);
+
+            assert.equal(out, 2);
+            fin();
+          });
+        },
+
+        only_incr_int0: function(fin) {
+          cache.incr({key: 'a', val: 1}, function(err, out) {
+            assert.ok(err)
+            fin();
+          });
+        },
+
+
+        only_decr_int0: function(fin) {
+          cache.decr({key: 'a', val: 1}, function(err, out) {
+            assert.ok(err)
+            fin();
+          });
+        },
+
+        delete0: function(fin) {
+          cache.delete({key: 'a'}, function(err, out) {
+            if(err) return fin(err);
+            assert(out, 'a');
+
+            cache.has({key: 'a'}, function(err, out) {
+              if(err) return fin(err);
+              assert.equal(out, false);
+              fin();
+            });
+          });
+        },
+
+        delete1: function(fin) {
+          cache.delete({key: 'b'}, function(err, out) {
+            if(err) return fin(err);
+            assert(out, 'a');
+
+            cache.has({key: 'b'}, function(err, out) {
+              if(err) return fin(err);
+              assert.equal(out, false);
+              fin();
+            });
+          });
+        }
+      },
+      done)
+  })
+}
+
