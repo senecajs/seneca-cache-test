0b36f14742c6848005e4eedeece32fc919b9d6bc
Richard Rodger <richard@ricebridge.com>
2014-12-05 00:25:03 +0000

    bump 0.2.0

--- cache-test.js
+++ cache-test.js
@@ -13,105 +13,132 @@ exports.basictest = function(si,done) {
     console.log('BASIC')
     assert.ok( si )
 
-    var cache = si.pin({role: 'cache', cmd: '*'});
+    var cache = si.pin({role: 'cache', cmd: '*'})
 
     async.series(
       {
         init0: function(fin) {
           cache.delete({key: 'a'}, function(err, out) {
-            if(err) return fin(err);
+            if(err) return fin(err)
 
             cache.delete({key: 'b'}, function(err, out) {
-              if(err) return fin(err);
-              fin();
-            });
-          });
+              if(err) return fin(err)
+              fin()
+            })
+          })
         },
 
         set0: function(fin) {
           cache.set({key: 'a', val: '1'}, function(err, out) {
-            if(err) return fin(err);
-            assert(out, 'a');
-            fin();
-          });
+            if(err) return fin(err)
+            assert(out, 'a')
+            fin()
+          })
         },
 
         get0: function(fin) {
           cache.get({key: 'a'}, function(err, out) {
-            if(err) return fin(err);
-            assert.equal(out, '1');
-            fin();
-          });
+            if(err) return fin(err)
+            assert.equal(out, '1')
+            fin()
+          })
         },
 
 
         add0: function(fin) {
           cache.add({key: 'b', val: 2}, function(err, out) {
-            if(err) return fin(err);
-            assert.equal(out, 'b');
-            fin();
-          });
+            if(err) return fin(err)
+            assert.equal(out, 'b')
+            fin()
+          })
         },
 
         refuse_existing0: function(fin) {
           cache.add({key: 'b', val: 'something'}, function(err, out) {
-            if(!err) return fin(new Error('refuse_existing0'));
+            if(!err) return fin(new Error('refuse_existing0'))
 
             cache.get({key: 'b'}, function(err, out) {
-              if(err) return fin(err);
-              assert.equal(out, 2);
-              fin();
-            });
-          });
+              if(err) return fin(err)
+              assert.equal(out, 2)
+              fin()
+            })
+          })
         },
 
         incr0: function(fin) {
           cache.incr({key: 'b', val: 4}, function(err, out) {
-            if(err) return fin(err);
+            if(err) return fin(err)
 
-            assert.equal(out, 6);
-            fin();
-          });
+            assert.equal(out, 6)
+            fin()
+          })
         },
 
         decr0: function(fin) {
           cache.decr({key: 'b', val: 3}, function(err, out) {
-            if(err) return fin(err);
+            if(err) return fin(err)
 
-            assert.equal(out, 3);
-            fin();
-          });
+            assert.equal(out, 3)
+            fin()
+          })
+        },
+
+        val0: function(fin) {
+          cache.set({key: 'z', val: 0}, function(err, out) {
+            if(err) return fin(err)
+            assert(out, 0)
+
+            cache.incr({key: 'z', val: 1}, function(err, out) {
+              if(err) return fin(err)
+              assert(out, 1)
+            
+              fin()
+            })
+          })
         },
 
         delete0: function(fin) {
           cache.delete({key: 'a'}, function(err, out) {
-            if(err) return fin(err);
-            assert(out, 'a');
+            if(err) return fin(err)
+            assert(out, 'a')
 
             cache.get({key: 'a'}, function(err, out) {
-              if(err) return fin(err);
-              assert.ok(!out);
-              fin();
-            });
-          });
+              if(err) return fin(err)
+              assert.ok(!out)
+              fin()
+            })
+          })
         },
 
         delete1: function(fin) {
           cache.delete({key: 'b'}, function(err, out) {
-            if(err) return fin(err);
-            assert(out, 'a');
+            if(err) return fin(err)
+            assert(out, 'a')
 
             cache.get({key: 'b'}, function(err, out) {
-              if(err) return fin(err);
-              assert.ok(!out);
-              fin();
-            });
-          });
+              if(err) return fin(err)
+              assert.ok(!out)
+              fin()
+            })
+          })
+        },
+
+        delete3: function(fin) {
+          cache.delete({key: 'z'}, function(err, out) {
+            if(err) return fin(err)
+            assert(out, 1)
+
+            cache.get({key: 'z'}, function(err, out) {
+              if(err) return fin(err)
+              assert.ok( null == out )
+              fin()
+            })
+          })
         },
 
         native0: function(fin){
           si.act('role:cache,get:native',function(err,native){
-            if(err) return fin(err);
+            if(err) return fin(err)
             assert.ok(native)
             fin()
           })
