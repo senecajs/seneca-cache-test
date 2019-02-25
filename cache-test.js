3b9069298eae58349ebb0b5df6e81f7b06ec9a61
rjrodger <richard@ricebridge.com>
2019-02-25 19:40:34 +0000

    v1.2.0

--- cache-test.js
+++ cache-test.js
@@ -8,13 +8,18 @@ exports.basictest = function(si, done) {
   si.ready(function() {
     Assert.ok(si)
 
+    var ka = 'a-' + Math.random()
+    var kb = 'b-' + Math.random()
+    var kq = 'q-' + Math.random()
+    var kz = 'z-' + Math.random()
+
     Async.series(
       {
         init0: function(fin) {
-          si.act('role:cache,cmd:delete', { key: 'a' }, function(err, out) {
+          si.act('role:cache,cmd:delete', { key: ka }, function(err, out) {
             if (err) return fin(err)
 
-            si.act('role:cache,cmd:delete', { key: 'b' }, function(err, out) {
+            si.act('role:cache,cmd:delete', { key: kb }, function(err, out) {
               if (err) return fin(err)
               fin()
             })
@@ -22,18 +27,18 @@ exports.basictest = function(si, done) {
         },
 
         set0: function(fin) {
-          si.act('role:cache,cmd:set', { key: 'a', val: '1' }, function(
+          si.act('role:cache,cmd:set', { key: ka, val: '1' }, function(
             err,
             out
           ) {
             if (err) return fin(err)
-            Assert(out, 'a')
+            Assert(out, ka)
             fin()
           })
         },
 
         get0: function(fin) {
-          si.act('role:cache,cmd:get', { key: 'a' }, function(err, out) {
+          si.act('role:cache,cmd:get', { key: ka }, function(err, out) {
             if (err) return fin(err)
             Assert.equal(out.value, '1')
             fin()
@@ -41,24 +46,21 @@ exports.basictest = function(si, done) {
         },
 
         add0: function(fin) {
-          si.act('role:cache,cmd:add', { key: 'b', val: 2 }, function(
-            err,
-            out
-          ) {
+          si.act('role:cache,cmd:add', { key: kb, val: 2 }, function(err, out) {
             if (err) return fin(err)
-            Assert.equal(out.key, 'b')
+            Assert.equal(out.key, kb)
             fin()
           })
         },
 
         refuse_existing0: function(fin) {
-          si.act('role:cache,cmd:add', { key: 'b', val: 'something' }, function(
+          si.act('role:cache,cmd:add', { key: kb, val: 'something' }, function(
             err,
             out
           ) {
             if (!err) return fin(new Error('refuse_existing0'))
 
-            si.act('role:cache,cmd:get', { key: 'b' }, function(err, out) {
+            si.act('role:cache,cmd:get', { key: kb }, function(err, out) {
               if (err) return fin(err)
               Assert.equal(out.value, 2)
               fin()
@@ -67,7 +69,7 @@ exports.basictest = function(si, done) {
         },
 
         incr0: function(fin) {
-          si.act('role:cache,cmd:incr', { key: 'b', val: 4 }, function(
+          si.act('role:cache,cmd:incr', { key: kb, val: 4 }, function(
             err,
             out
           ) {
@@ -80,7 +82,7 @@ exports.basictest = function(si, done) {
 
         // key does not exist
         incr1: function(fin) {
-          si.act('role:cache,cmd:incr', { key: 'q', val: 1 }, function(
+          si.act('role:cache,cmd:incr', { key: kq, val: 1 }, function(
             err,
             out
           ) {
@@ -92,7 +94,7 @@ exports.basictest = function(si, done) {
         },
 
         decr0: function(fin) {
-          si.act('role:cache,cmd:decr', { key: 'b', val: 3 }, function(
+          si.act('role:cache,cmd:decr', { key: kb, val: 3 }, function(
             err,
             out
           ) {
@@ -104,14 +106,11 @@ exports.basictest = function(si, done) {
         },
 
         val0: function(fin) {
-          si.act('role:cache,cmd:set', { key: 'z', val: 0 }, function(
-            err,
-            out
-          ) {
+          si.act('role:cache,cmd:set', { key: kz, val: 0 }, function(err, out) {
             if (err) return fin(err)
             Assert(out, 0)
 
-            si.act('role:cache,cmd:incr', { key: 'z', val: 1 }, function(
+            si.act('role:cache,cmd:incr', { key: kz, val: 1 }, function(
               err,
               out
             ) {
@@ -124,11 +123,11 @@ exports.basictest = function(si, done) {
         },
 
         delete0: function(fin) {
-          si.act('role:cache,cmd:delete', { key: 'a' }, function(err, out) {
+          si.act('role:cache,cmd:delete', { key: ka }, function(err, out) {
             if (err) return fin(err)
-            Assert(out, 'a')
+            Assert(out, ka)
 
-            si.act('role:cache,cmd:get', { key: 'a' }, function(err, out) {
+            si.act('role:cache,cmd:get', { key: ka }, function(err, out) {
               if (err) return fin(err)
               Assert.ok(!out.value)
               fin()
@@ -137,11 +136,11 @@ exports.basictest = function(si, done) {
         },
 
         delete1: function(fin) {
-          si.act('role:cache,cmd:delete', { key: 'b' }, function(err, out) {
+          si.act('role:cache,cmd:delete', { key: kb }, function(err, out) {
             if (err) return fin(err)
-            Assert(out, 'a')
+            Assert(out, ka)
 
-            si.act('role:cache,cmd:get', { key: 'b' }, function(err, out) {
+            si.act('role:cache,cmd:get', { key: kb }, function(err, out) {
               if (err) return fin(err)
               Assert.ok(!out.value)
               fin()
@@ -150,11 +149,11 @@ exports.basictest = function(si, done) {
         },
 
         delete3: function(fin) {
-          si.act('role:cache,cmd:delete', { key: 'z' }, function(err, out) {
+          si.act('role:cache,cmd:delete', { key: kz }, function(err, out) {
             if (err) return fin(err)
             Assert(out, 1)
 
-            si.act('role:cache,cmd:get', { key: 'z' }, function(err, out) {
+            si.act('role:cache,cmd:get', { key: kz }, function(err, out) {
               if (err) return fin(err)
               Assert.ok(null == out.value)
               fin()
